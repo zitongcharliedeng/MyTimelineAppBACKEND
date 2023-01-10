47ad89b85c76d16a950127010ad8f671bdc5fc69
charlie <charlie@example.com>
2023-01-10 15:13:03 +0000

    fixed image display

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,8 +2,12 @@ class ImagesController < ApplicationController
   def create
     @image = Image.new()
     @image.save
-    @image.image.attach(params[:image])
-    @image_url = url_for(@image.image)
-    render json: {imageUrl: @image_url}
+    if (params[:image] != "null")
+      @image.image.attach(params[:image])
+      @image_url = url_for(@image.image)
+      render json: {imageUrl: @image_url}
+    else
+      render json: {imageUrl: "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"}
+    end
   end
 end
